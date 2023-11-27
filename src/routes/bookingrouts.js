6cbdf14bebb91945d6327630cf60048cc457cae2
Your Name <you@example.com>
2023-11-27 11:05:06 +0530

    Ajithkumar

--- src/routes/bookingrouts.js
+++ src/routes/bookingrouts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express.Router();
 const db = require("../db/db");
 
-app.put("/trans_boooking/:bookingId/", (req, res) => {
+app.put("/trans_booking/:bookingId/", (req, res) => {
   const bookingId = req.params.bookingId;
   const { Payment_status } = req.body;
 
@@ -37,7 +37,7 @@ app.post('/trans_booking/:user_id', (req, res) => {
     to_longitude,
     to_Place,
     km_distance,
-    is_conform,
+    is_confirm,
     driver_id,
     Amount,
     Coupon_amount,
@@ -51,10 +51,10 @@ app.post('/trans_booking/:user_id', (req, res) => {
   const toPlaceString = typeof to_Place === 'object' ? JSON.stringify(to_Place) : to_Place;
 
   db.query(
-    'INSERT INTO trans_booking (booking_date, user_id, from_latitude, from_longitude, from_Place, to_latitude, to_longitude, to_Place, km_distance, is_conform, driver_id, Amount, Coupon_amount, total_amount, Payment_status, booking_status, vehicle_Id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+    'INSERT INTO trans_booking (booking_date, user_id, from_latitude, from_longitude, from_Place, to_latitude, to_longitude, to_Place, km_distance, is_confirm, driver_id, Amount, Coupon_amount, total_amount, Payment_status, booking_status, vehicle_Id) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
     [
       booking_date,
-      user_id, // Use user_id instead of bookingId
+      user_id, 
       from_latitude,
       from_longitude,
       fromPlaceString,
@@ -62,7 +62,7 @@ app.post('/trans_booking/:user_id', (req, res) => {
       to_longitude,
       toPlaceString,
       km_distance,
-      is_conform,
+      is_confirm,
       driver_id,
       Amount,
       Coupon_amount,
@@ -81,6 +81,7 @@ app.post('/trans_booking/:user_id', (req, res) => {
     }
   );
 });
+
 
 app.post('/mas_vehicles', (req, res) => {
   const vehicle = req.body;
@@ -145,6 +146,176 @@ app.get('/vehicle_charge', (req, res) => {
     res.json({ charge });
   }
 });
+//================================faremanagement=======================
+// app.post('/flat_fare', (req, res) => {
+//   const flatfare = req.body;
+//  db.query('INSERT INTO flat_fare SET ?', flatfare, (error, results) => {
+//     if (error) {
+//       console.error('Error inserting flat_fare data:', error);
+//       res.status(500).json({ error: 'Internal server error' });
+//     } else {
+//       res.json({ message: ' flatfare successfully' });
+//     }
+//   });
+// });
 
 
+
+
+
+app.get('/flatfare', (req, res) => {
+  const query = 'SELECT *  FROM flat_fare';
+
+  db.query(query, (err, results) => {
+      if (err) {
+          res.status(500).json({ error: 'Database error' });
+          return;
+      }
+
+      res.json(results);
+  });
+});
+app.get('/rangefare', (req, res) => {
+  const query = 'SELECT *  FROM range_fare';
+
+  db.query(query, (err, results) => {
+      if (err) {
+          res.status(500).json({ error: 'Database error' });
+          return;
+      }
+
+      res.json(results);
+  });
+});
+
+app.post('/flat_fare', (req, res) => {
+  const q = "INSERT INTO flat_fare(`vehicle_Type`, `per_km`, `Amount`) VALUES(?)";
+  const values = [
+      req.body.vehicle_Type,
+      req.body.per_km,
+      req.body.Amount,];
+  db.query(q, [values], (err) => {
+      if (err) return res.json(err);
+     return res.status(200).json("flatfare add successfully.");
+  });
+});
+
+app.delete('/flat_fare/:id', (req, res) => {
+  const flatFareId = req.params.id;
+
+  db.query('SELECT * FROM flat_fare WHERE flat_fare_id = ?', [flatFareId], (err, results) => {
+    if (err) {
+      console.error('Error checking if flat_fare data exists:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ error: 'Id not found' });
+      } else {
+        db.query('DELETE FROM flat_fare WHERE flat_fare_id = ?', [flatFareId], (err, deleteResults) => {
+          if (err) {
+            console.error('Error deleting flat_fare data:', err);
+            res.status(500).json({ error: 'Internal server error' });
+          } else {
+           
+            res.status(200).json({ id: flatFareId, message: 'Successfully deleted' });
+          }
+        });
+      }
+    }
+  });
+});
+
+
+
+
+app.put('/flat_fare/:id', (req, res) => {
+  const flatFareId = req.params.id;
+  const updatedData = req.body; 
+
+  db.query('SELECT * FROM flat_fare WHERE flat_fare_id = ?', [flatFareId], (err, preUpdateResults) => {
+    if (err) {
+      console.error('Error checking if flat_fare data exists:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      if (preUpdateResults.length === 0) {
+        res.status(404).json({ error: 'Id not found' });
+      } else {
+       
+        db.query('UPDATE flat_fare SET ? WHERE flat_fare_id = ?', [updatedData, flatFareId], (err, updateResults) => {
+          if (err) {
+            console.error('Error updating flat_fare data:', err);
+            res.status(500).json({ error: 'Internal server error' });
+          } else {
+                res.status(200).json({ id: flatFareId, message: 'Update successful' });
+          }
+        });
+      }
+    }
+  });
+});
+
+app.post('/Range_fare', (req, res) => {
+  const q = "INSERT INTO range_fare(`vehicle_Type`, `from_km`, `to_km`,`Amount`) VALUES(?)";
+  const values = [
+      req.body.vehicle_Type,
+      req.body.from_km,
+      req.body.to_km,
+      req.body.Amount,];
+  db.query(q, [values], (err) => {
+      if (err) return res.json(err);
+     return res.status(200).json("Rangefare add successfully.");
+  });
+});
+
+
+app.delete('/range_fare/:id', (req, res) => {
+  const rangeFareId = req.params.id;
+
+  db.query('SELECT * FROM range_fare WHERE range_fare_id = ?', [rangeFareId], (err, results) => {
+    if (err) {
+      console.error('Error checking if flat_fare data exists:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      if (results.length === 0) {
+        res.status(404).json({ error: 'Id not found' });
+      } else {
+        db.query('DELETE FROM range_fare WHERE range_fare_id = ?', [rangeFareId], (err, deleteResults) => {
+          if (err) {
+            console.error('Error deleting flat_fare data:', err);
+            res.status(500).json({ error: 'Internal server error' });
+          } else {
+           
+            res.status(200).json({ id: rangeFareId, message: 'Successfully deleted' });
+          }
+        });
+      }
+    }
+  });
+});
+
+app.put('/range_fare/:id', (req, res) => {
+  const rangeFareId = req.params.id;
+  const updatedData = req.body; 
+
+  db.query('SELECT * FROM range_fare WHERE range_fare_id = ?', [rangeFareId], (err, preUpdateResults) => {
+    if (err) {
+      console.error('Error checking if range_fare data exists:', err);
+      res.status(500).json({ error: 'Internal server error' });
+    } else {
+      if (preUpdateResults.length === 0) {
+        res.status(404).json({ error: 'Id not found' });
+      } else {
+       
+        db.query('UPDATE range_fare SET ? WHERE range_fare_id = ?', [updatedData, rangeFareId], (err, updateResults) => {
+          if (err) {
+            console.error('Error updating range_fare data:', err);
+            res.status(500).json({ error: 'Internal server error' });
+          } else {
+                res.status(200).json({ id: rangeFareId, message: 'Update successful' });
+          }
+        });
+      }
+    }
+  });
+});
 module.exports = app; 
